a2c4edf0f0d7b4754f89b8e12cfe8d2cdd1f1b3a
Anshika Pandey <pandeyanshika6010@gmail.com>
2026-06-18 20:16:10 +0530

    "Added Normal Prime Code"

---
+++ Prime.java
@@ -0,0 +1,19 @@
+public class Prime {
+    public static void main(String[] args) {
+        int n = 29;
+        int count = 0 ;
+        for ( int i = 1; i <= n; i++){
+            if (n % i == 0){
+                count++;
+            }
+        }
+        if(count == 2){
+            System.out.println(n +" is a prime number");
+        }else {
+            System.out.println(n + " not a prime number");
+        }
+       
+        }
+    }
+    
+
